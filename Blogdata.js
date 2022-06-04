a3d9af17441a6f7859ce5bb1bdc57f641c7affd5
MORRIS MUTISYA SAMMY <morrehjayammsgk@gmail.com>
2022-06-04 11:35:52 +0300

    website under react js

---
+++ Blogdata.js
@@ -0,0 +1,25 @@
+const Blogdata = [
+    {
+      id: 1,
+      date: "01 JUNE, 2022",
+      title: "The Most Popular New top Business Apps",
+      cover: "./assets/blog/blog1.jpg",
+      category: "Technology",
+    },
+    {
+      id: 2,
+      date: "02 JUNE, 2022",
+      title: "The Best Marketing top Management Tools",
+      cover: "./assets/blog/blog2.jpg",
+      category: "Agency",
+    },
+    {
+      id: 3,
+      date: "03 JUNE, 2022",
+      title: "3 WooCommerce Plugins to Boost Sales",
+      cover: "./assets/blog/blog3.jpg",
+      category: "IT",
+    },
+  ]
+  
+  export default Blogdata
